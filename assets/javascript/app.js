37b9db382690a8e8c0865fd5db882c5fef9bc424
john Meng <john.meng014@gmail.com>
2019-09-12 15:10:09 -0400

    got the quesitons to appear and times to run and slect answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,7 +80,141 @@ $(document).ready(function(){
     ]
     ]
 
+    var nextQuestion = 0;//counter for the purpose of the next question
+    var answerVar = questions[nextQuestion][5], answerText = questions[nextQuestion][7];
+    var intervalId;
+    var number = 30; // the start of each timer
+    var imagesHere = questions[nextQuestion][6];
+    var incorrect = 0, correct = 0, restarting = true;
 
+        //hiding the "time remaining" portion in the html
+        $(".RT").hide();
+        $("button").on("click", function () {
+            // if the restarting is false, it will go into the restart function and zero everything. This was necessary because without this, every time I restarted the game, it counted down starting at zero and into the negatives.
+            if (restarting === false) {
+                restarting = true;
+                restart();
+            }
+            $(".RT").show();
+            start();
+            $("#choices, #question").show();
+    
+        })
+        // start function that will start the countdown and then goes to the 'next' function
+        function start() {
+            $("button").hide()
+            intervalId = setInterval(decrement, 1000);
+            next();
+        }
+        //This function shows the question from the array.  The question goes in order
+        function next() {
+    
+            $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
+            $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
+            $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
+            $("#c").html("<h4 value='c'>" + questions[nextQuestion][3] + "</h4>");
+            $("#d").html("<h4 value='d'>" + questions[nextQuestion][4] + "</h4>");
+        }
+    
+        // click function which adds the correct and incorrect answers
+        $("#a, #b, #c, #d").click(function () {
+    
+            var a = $(this).text();
+            if (a === answerVar) {
+                correct++;
+                number = 30;
+                stop();
+                right();
+            } else {
+                incorrect++;
+                stop();
+                sorry();
+            }
+    
+        })
+        // every time I get a question correct, it will take me to this function which says you've got the question right and some pictures.  This will also have a setTimeout function which will wait about 3 seconds and then move on to the next question.
+        function right() {
+            if (nextQuestion == questions.length - 1) {
+                imagesHere = questions[nextQuestion][6]; 
+                answerText = questions[nextQuestion][7];
+                $("#choices").hide();
+                $("#question").html("<h1> Correct! <br>" + answerText + "</h1>");
+                $("#images").show().html(imagesHere);
+    
+                setTimeout(function () {
+                    $("#question, #choices, #images").hide();
+                    gameOver();
+                }, 3000);
+            }
+            imagesHere = questions[nextQuestion][6];
+            answerText = questions[nextQuestion][7];
+            $("#choices").hide();
+            $("#question").html("<h1> Yes, sir!" + answerText + "</h1>");
+            $("#images").show().html(imagesHere);
+    
+            nextQuestion++;// adding 1 so that we can move on to the next question in the array at the top
+            answerVar = questions[nextQuestion][5];
+            setTimeout(function () {
+                start();// this will start the counter again
+                $("#images").hide();
+                $("#choices").show();
+                $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
+                $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
+                $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
+                $("#c").html("<h4 value='c'>" + questions[nextQuestion][3] + "</h4>");
+                $("#d").html("<h4 value='d'>" + questions[nextQuestion][4] + "</h4>");
+            }, 3000);
+        }
+    
+        
+        function sorry() {
+            //this will show if you got the last question wrong and then go over to the gameOver function
+            if (nextQuestion == questions.length - 1) {
+                imagesHere = questions[nextQuestion][6];
+                answerText = questions[nextQuestion][7];
+                $("#choices").hide();
+                $("#question").html("<h1> Sorry!<br>" + answerText + "</h1>");
+                $("#images").show().html(imagesHere);
+    
+                setTimeout(function () {
+                    $("#question, #choices, #images").hide();
+                    gameOver();
+                }, 3000);
+            }
+    
+            imagesHere = questions[nextQuestion][6];
+            answerText = questions[nextQuestion][7];
+            $("#choices").hide();
+            $("#question").html("<h1> Sorry, The right answer is <br>" + answerText + "</h1>");
+            $("#images").show().html(imagesHere);
+    
+            nextQuestion++;
+            answerVar = questions[nextQuestion][5];
+    
+            setTimeout(function () {
+                start();
+                $("#images").hide();
+                $("#choices").show();
+                $("#question").html("<h3>" + questions[nextQuestion][0] + "</h3>");
+                $("#a").html("<h4 value='a'>" + questions[nextQuestion][1] + "</h4>");
+                $("#b").html("<h4 value='b'>" + questions[nextQuestion][2] + "</h4>");
+                $("#c").html("<h4 value='c'>" + questions[nextQuestion][3] + "</h4>");
+                $("#d").html("<h4 value='d'>" + questions[nextQuestion][4] + "</h4>");
+            }, 3000);
+    
+        }
+        //function to count down from the remaining time
+        function decrement() {
+            number--;
+            $("#timer").text(number);
+            if (number === 0) {
+                stop();
+                $("#question, #choices").hide();
+                $("#timer").html("<h2>Times Run Out! Start Over!</h2>");
+                restarting = false;
+                $("#start").show();
+            }
+        }
 
 
 
