f792394ccabfc707ff4f6ab5383434b543e649ca
john Meng <john.meng014@gmail.com>
2019-09-12 13:33:57 -0400

    added all the trivia quesitons in JS file with image

---
+++ assets/javascript/app.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+    // creating an array for the questions
+    var questions = [[
+        "What pokemon is the evolved form of Pikachu",
+        "A) Jolteon",
+        "B) Electrobuzz",
+        "C) Raichu",
+        "D) Meowth",
+        "C) Raichu",
+        "<img src='assets/images/Raichu.png'>", "Raichu is the evolved form."
+    ],
+    [
+        "What is the best type of pokeball",
+        "A) Ultra Ball",
+        "B) Master Ball",
+        "C) Great Ball",
+        "D) Legendary Ball",
+        "B) Master Ball",
+        "<img src='assets/images/Master.jpg'>", "Master Ball is the best type."
+    ], [
+        "Who is the main character in the original Pokemon television series?",
+        "A) Brook",
+        "B) Misty",
+        "C) Gary",
+        "D) Ash",
+        "D) Ash",
+        "<img src='assets/images/ash.jpeg'>", "Ash is the main character!"
+    ], [
+        "How many Gym Badges do you need before challenging the elite four",
+        "A) 6", "B) 7", "C) 8", "D) 9", "C) 8", "<img src='assets/images/badges.png'>", "8 Gym Badges"
+    ], [
+        "Where do you go to revive all your pokemon to full health?",
+        "A) Poke Mart",
+        "B) Poke spot",
+        "C) Pokement Hostpital",
+        "D) Pokemon Center",
+        "D) Pokemon Center",
+        "<img src='assets/images/Center.jpg'>", "Pokemon Center."
+    ], [
+        "What are the three types from the starter Pokemon",
+        "A) Fire, Water, Grass",
+        "B) Rock, Flying, Normal",
+        "C) Electric, Fighting, Psychic",
+        "D) Normal, Fighting, Water",
+        "A) Fire, Water, Grass",
+        "<img src='assets/images/starter.jpg'>", "Fire, Water, Grass are the starting types"
+    ], [
+        "Which Pokemon was known for its punching?",
+        "A) Hitmonchan",
+        "B) Hitmonlee",
+        "C) Snorlax",
+        "D) Machamp",
+        "A) Hitmonchan",
+        "<img src='assets/images/hitmonchan.jpg'>", "Hitmonchan"
+    ], [
+        "What is the name of Ash's Rival",
+        "A) Giovanni",
+        "B) Gary",
+        "C) Team Rocket",
+        "D) Brock",
+        "B) Gary",
+        "<img src='assets/images/Gary.png'>", "Gary!"
+    ], [
+        "What generation was Lugia introduced?",
+        "A) First Generation",
+        "B) Third Generation",
+        "C) Fourth Generation",
+        "D) Second Generation",
+        "D) Second Generation",
+        "<img src='assets/images/Lugia.png'>", "Second Generation."
+    ],
+    [
+        "What pokemon do team rocket always travel with?",
+        "A) Muk",
+        "B) Butterfree",
+        "C) Pikachu",
+        "D) Meowth",
+        "D) Meowth",
+        "<img src='assets/images/meowth.png'>", "Meowth."
+    ]
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
